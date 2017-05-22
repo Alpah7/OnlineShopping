fdc3e8f1e28c14ab6b166aaec2ca955148aa9cd8
Ratnasetya <ratnasetya209@gmail.com>
2017-05-23 02:59:01 +0700

    Updated

--- admin/play.php
+++ admin/play.php
@@ -1,12 +1,35 @@
 <?php 
 
-$date = new DateTime();
-$now = $date->format('F-Y');
-echo $now;
+// $date = new DateTime();
+// $now = $date->format('F-Y');
+// echo $now;
 
 // $idOrder = 'ORD-0417-17-2';
 
-// $conn = new MySQLi('localhost','root','','project_ecommerce');
+$conn = new MySQLi('localhost','root','','project_ecommerce');
+
+$query_select = "SELECT
+	SUM(Total_Data) AS TotalTransaction,
+	SUM(Gross) AS TotalGrossIncome,
+	SUM(Net) AS TotalNetIncome
+FROM (
+		SELECT COUNT(id_product) AS Total_Data,
+		SUM(gross_income) AS Gross,
+		SUM(net_income) AS Net
+		FROM transactions
+			WHERE DATE_FORMAT(date_transaction, '%Y-%m')
+  		BETWEEN '2017-01' AND DATE_FORMAT(date_transaction, '%Y-%m')
+  		GROUP BY id_transaction, DATE_FORMAT(date_transaction, '%Y-%m') 
+  		ORDER BY DATE_FORMAT(date_transaction, '%Y-%m') 
+) AS DataTotal";
+
+$sql_select = $conn->query($query_select);
+$data = array();
+$result_select = $sql_select->fetch_all(MYSQLI_ASSOC);
+$earnings = ($result_select[0]['TotalGrossIncome']-$result_select[0]['TotalNetIncome']);
+
+return $earnings;
+
 
 // $query_select = "SELECT status FROM order_product WHERE id_order='".$idOrder."'";
 // $sql_select = $conn->query($query_select);
